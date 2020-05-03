4238af3f3882e9ffaf4a7e9e47cd93e6ddff81e3
Steve Brown <steve.brown@thebrownnet.co.uk>
2020-05-03 15:39:39 +0100

    fix nadgered firewall rules

--- modules/example_puppetserver/manifests/firewall.pp
+++ modules/example_puppetserver/manifests/firewall.pp
@@ -2,19 +2,45 @@
 #
 # Configures the firewall on the puppet server to allow expected services.
 class example_puppetserver::firewall {
-  firewall { '001 accept SSH':
+  firewall { '000 accept all icmp':
+    proto   => 'icmp',
+    action  => 'accept',
+    require => Package['iptables-persistent'],
+  }
+  -> firewall { '001 accept all to lo interface':
+    proto   => 'all',
+    iniface => 'lo',
+    action  => 'accept',
+  }
+  -> firewall { '002 reject local traffic not on loopback interface':
+    iniface     => '! lo',
+    proto       => 'all',
+    destination => '127.0.0.1/8',
+    action      => 'reject',
+  }
+  -> firewall { '003 accept related established rules':
+    proto  => 'all',
+    state  => ['RELATED', 'ESTABLISHED'],
+    action => 'accept',
+  }
+  -> firewall { '004 accept SSH':
       proto  => 'tcp',
       dport  => 22,
       action => accept,
   }
-  -> firewall { '002 allow 8081 (PuppetDB)':
+  -> firewall { '005 allow 8081 (PuppetDB)':
       proto  => 'tcp',
       dport  => 8081,
       action => accept,
   }
-  -> firewall { '003 allow 8140 (Puppet Agent Incoming)':
+  -> firewall { '006 allow 8140 (Puppet Agent Incoming)':
       proto  => 'tcp',
       dport  => 8140,
+      action => accept,
+  }
+  -> firewall { '007 accept HTTP (PuppetBoard)':
+      proto  => 'tcp',
+      dport  => 80,
       action => accept,
   }
   -> firewall { '999 drop all':
